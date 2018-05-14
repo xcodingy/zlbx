ffcfad7688e65dca86147d8703cb61d223f06cfd
xcodingy <xieyilun@sina.com>
2018-05-13 17:31:18 -0700

    index ui completed

--- src/component/Flow.js
+++ src/component/Flow.js
@@ -13,9 +13,19 @@ import arrow_l from '../res/arrow_l.png';
 
 // Arrow animation class
 class ArrowDiv extends Component {
+    constructor(args) {
+        super(args);
+
+        this.fadeInterval = 20;
+        this.stopInterval = 300;
+        this.fstInterval = this.fadeInterval * 10 + this.stopInterval;
+        this.secInterval = this.fstInterval + this.fadeInterval * 10 + this.stopInterval;
+        this.trdInterval = this.secInterval + this.fadeInterval * 10 + this.stopInterval;
+    }
+
     componentDidMount() {
         this.timerID = setInterval(              // set up arrow animation trigger
-            () => this.updateArrow(), 230
+            () => this.updateArrow(), 2300
         );
     }
 
@@ -24,21 +34,82 @@ class ArrowDiv extends Component {
     }
 
     updateArrow() {         // define arrow animation
-        let a = this.refs['arrow_1'].getAttribute('src');
-        let b = this.refs['arrow_2'].getAttribute('src');
-        let c = this.refs['arrow_3'].getAttribute('src');
+        let a = this.refs['arrow_1'];
+        let b = this.refs['arrow_2'];
+        let c = this.refs['arrow_3'];
 
-        this.refs['arrow_2'].setAttribute('src', a);
-        this.refs['arrow_3'].setAttribute('src', b);
-        this.refs['arrow_1'].setAttribute('src', c);
+        this.fadeArrowOut(a);
+        setTimeout(
+            () => this.fadeArrow(a, b), this.fstInterval
+        );
+        setTimeout(
+            () => this.fadeArrow(b, c), this.secInterval
+        );
+        setTimeout(
+            () => this.fadeArrowIn(c), this.trdInterval
+        );
     }
+
+    fadeArrow(arrow_a, arrow_b) {
+        let num = 0;
+
+        let fadeID = setInterval(
+            () => {
+                num ++;
+                arrow_a.style.opacity = num/10;
+                arrow_b.style.opacity = (10 - num)/10;
+
+                if (num === 10) {
+                    clearInterval(fadeID);
+                }
+            }, this.fadeInterval
+        );
+    }
+
+    fadeArrowIn(arrow) {
+        let num = 0;
+
+        let fadeID = setInterval(
+            () => {
+                num += 1;
+                arrow.style.opacity = num/10;
+
+                if (num === 10) {
+                    clearInterval(fadeID);
+                }
+            }, this.fadeInterval
+        )
+    }
+
+    fadeArrowOut(arrow) {
+        let num = 10;
+
+        let fadeID = setInterval(
+            () => {
+                num -= 1;
+                arrow.style.opacity = num/10;
+
+                if (num === 0) {
+                    clearInterval(fadeID);
+                }
+            }, this.fadeInterval
+        )
+    }
+
 
     render() {
         return (
             <div className='connector'>
-                <img alt='arrow' className='arrow_1' src={arrow_l} ref='arrow_1' />
-                <img alt='arrow' className='arrow_2' src={arrow} ref='arrow_2' />
-                <img alt='arrow' className='arrow_3' src={arrow} ref='arrow_3' />
+                <div className='arrow'>
+                    <img alt='arrow' className='arrow-1' src={arrow} ref='arrow_1' />
+                    <img alt='arrow' className='arrow-2' src={arrow} ref='arrow_2' />
+                    <img alt='arrow' className='arrow-3' src={arrow} ref='arrow_3' />
+                </div>
+                <div className='arrow-l'>
+                    <img alt='arrow' className='arrow-1' src={arrow_l} />
+                    <img alt='arrow' className='arrow-2' src={arrow_l} />
+                    <img alt='arrow' className='arrow-3' src={arrow_l} />
+                </div>
             </div>
         );
     }
@@ -61,7 +132,7 @@ class HexDiv extends Component {
         }
 
         this.state = {
-            div_class: 'hex_div ' + this.props.name,            // set up class state
+            div_class: 'hex-div ' + this.props.name,            // set up class state
             src: img_src
         };
     }
@@ -69,9 +140,9 @@ class HexDiv extends Component {
     render() {
         return (
             <div className={this.state.div_class}>
-                <div className='hex_text'>
-                    <span className='line_1'>{this.props.line1}</span>
-                    <span className='line_2'>{this.props.line2}</span>
+                <div className='hex-text'>
+                    <span className='line-1'>{this.props.line1}</span>
+                    <span className='line-2'>{this.props.line2}</span>
                 </div>
                 <img alt='border' src={this.state.src} />
             </div>
@@ -88,32 +159,32 @@ class Flow extends Component {
     render() {
         return (
             <div className='flow-container container'>
-                <div className='flow_1 flow'>
-                    <div className='flow_title'>
+                <div className='flow-1 flow'>
+                    <div className='flow-title'>
                         在线投保流程
                     </div>
-                    <HexDiv line1={this.line1_texts[1]} line2={this.line2_texts[1]} name='hex_div_0' />
+                    <HexDiv line1={this.line1_texts[1]} line2={this.line2_texts[1]} name='hex-div-0' />
                     <ArrowDiv />
-                    <HexDiv line1={this.line1_texts[2]} line2={this.line2_texts[2]} name='hex_div_1' />
+                    <HexDiv line1={this.line1_texts[2]} line2={this.line2_texts[2]} name='hex-div-1' />
                     <ArrowDiv />
-                    <HexDiv line1={this.line1_texts[3]} line2={this.line2_texts[3]} name='hex_div_2' />
+                    <HexDiv line1={this.line1_texts[3]} line2={this.line2_texts[3]} name='hex-div-2' />
                     <ArrowDiv />
-                    <HexDiv line1={this.line1_texts[4]} line2={this.line2_texts[4]} name='hex_div_3' flow='1' />
+                    <HexDiv line1={this.line1_texts[4]} line2={this.line2_texts[4]} name='hex-div-3' flow='1' />
                 </div>
                 <div className='delimiter'>
                     <img alt='delimiter' src={delimiter} />
                 </div>
-                <div className='flow_2 flow'>
-                    <div className='flow_title'>
+                <div className='flow-2 flow'>
+                    <div className='flow-title'>
                         微信投保流程
                     </div>
-                    <HexDiv line1={this.line1_texts[0]} line2={this.line2_texts[0]} name='hex_div_0' />
+                    <HexDiv line1={this.line1_texts[0]} line2={this.line2_texts[0]} name='hex-div-0' />
                     <ArrowDiv />
-                    <HexDiv line1={this.line1_texts[2]} line2={this.line2_texts[2]} name='hex_div_1' />
+                    <HexDiv line1={this.line1_texts[2]} line2={this.line2_texts[2]} name='hex-div-1' />
                     <ArrowDiv />
-                    <HexDiv line1={this.line1_texts[3]} line2={this.line2_texts[3]} name='hex_div_2' />
+                    <HexDiv line1={this.line1_texts[3]} line2={this.line2_texts[3]} name='hex-div-2' />
                     <ArrowDiv />
-                    <HexDiv line1={this.line1_texts[4]} line2={this.line2_texts[4]} name='hex_div_3' flow='2' />
+                    <HexDiv line1={this.line1_texts[4]} line2={this.line2_texts[4]} name='hex-div-3' flow='2' />
                 </div>
             </div>
         );
---
+++ src/component/Modal.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import '../css/Modal.css';
+
+
+class Modal extends Component {
+    render() {
+        return (
+            <div className='modal'>
+                <div className='modal-content'>
+
+                </div>
+                <div className='modal-background'>
+
+                </div>
+            </div>
+        );
+    }
+}
